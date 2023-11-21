a818891079cbf5bccfb993c854b297d31bd3ff5e
canxin <1969730106@qq.com>
2023-11-21 15:32:25 +0800

    add openocd with cortex-debug.

---
+++ readme.md
@@ -0,0 +1,17 @@
+# Requirements
+
+binarys
+
+1. OpenOCD
+2. gdb
+3. llvm-tools-preview
+4. cargo-binutils
+5. probe-rs
+6. defmt-print
+
+vscode extensions
+
+1. Shell Debugger
+2. Task Runner
+3. Cortex-Debug
+4. Debugger for probe-rs
--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,11 +1,11 @@
 //! Quick Start for STM32 H743
 #![no_std]
 #![no_main]
-use defmt::{error, info, println, warn};
+use defmt;
 use defmt_rtt as _;
 use panic_probe as _;
 
-use stm32h7xx_hal::prelude::*;
+use stm32h7xx_hal::{gpio, prelude::*};
 
 use cortex_m_rt::entry;
 
@@ -20,5 +20,31 @@ fn main() -> ! {
 
     let ccdr = rcc.sysclk(120.MHz()).freeze(pwrcfg, &dp.SYSCFG);
 
-    loop {}
+    let gpiob = dp.GPIOB.split(ccdr.peripheral.GPIOB);
+    let mut led_green = gpiob.pb0.into_push_pull_output();
+    let mut led_red = gpiob.pb14.into_push_pull_output();
+
+    // Also, we can set pin speed.
+    led_red.set_speed(gpio::Speed::High);
+
+    let mut delay = cp.SYST.delay(ccdr.clocks);
+    defmt::info!("This is a Info.");
+    defmt::warn!("This is a warn");
+    defmt::error!("This is a Error");
+    defmt::println!("This is just print");
+    loop {
+        // light up Yellow first
+        defmt::println!("Yellow");
+        led_green.set_high();
+        delay.delay_ms(500u32);
+        //  light up Red next
+        defmt::println!("Red");
+        led_red.set_high();
+        delay.delay_ms(500u16);
+
+        // led out after 500ms
+        led_green.set_low();
+        led_red.set_low();
+        delay.delay_ms(500u16);
+    }
 }
